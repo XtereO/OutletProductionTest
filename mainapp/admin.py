27793a43a25b5555f6291a32bc83859a890ec674
XtereO <71135845+XtereO@users.noreply.github.com>
2021-03-21 20:15:50 +0300

    FirstUp

---
+++ mainapp/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import (
+    Color,Shape,
+    Brand,GlassesRim,
+    Material,Gender,
+    Accessory,CategoryAccessory,
+    MarketPoint
+)
+# Register your models here.
+modelsToRegister=[
+    Color,Shape,Brand,
+    Material,GlassesRim,
+    Gender,CategoryAccessory,
+    Accessory,MarketPoint
+]
+for a in modelsToRegister:
+    admin.site.register(a)
