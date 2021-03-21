655ef7d73684bfa14d56743f025526e562711085
XtereO <71135845+XtereO@users.noreply.github.com>
2021-03-21 22:37:31 +0300

    update cors

--- Outlet/settings.py
+++ Outlet/settings.py
@@ -142,8 +142,10 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 
 CORS_ORIGIN_WHITELIST=[
+    "http://localhost:8000",
     "http://localhost:3000",
     "http://localhost:8080",
     "http://127.0.0.1:8000",
+    "https://outlettest.herokuapp.com",
 ]
 django_heroku.settings(locals())
