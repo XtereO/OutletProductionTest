79a10d3eb98a96d621d3d0aafe32202c27335135
XtereO <71135845+XtereO@users.noreply.github.com>
2021-03-21 22:24:33 +0300

    PullLastItems

--- Outlet/settings.py
+++ Outlet/settings.py
@@ -142,7 +142,8 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 
 CORS_ORIGIN_WHITELIST=[
+    "http://localhost:3000",
     "http://localhost:8080",
-    "http://127.0.0.0:8000",
+    "http://127.0.0.1:8000",
 ]
 django_heroku.settings(locals())
